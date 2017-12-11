145f207a9ec56bdc9b6c96fdf6b43b2093c8b015
Menna Ali <mennaali365@gmail.com>
2017-12-10 18:40:21 -0800

    Mennav2

--- src/java/Model/UserIO.java
+++ src/java/Model/UserIO.java
@@ -19,7 +19,7 @@ public class UserIO {
 
     String url = "jdbc:mysql://localhost:3306/house_buy_rent";
     String sqluser = "root";
-    String pass = "n33333";
+    String pass = "";
 
     public int getUserID(String userName) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
